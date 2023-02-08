e2f356e9982e2d89b38d3910a456af97383e28bf
Marcoagg255 <marcoguevara255@gmail.com>
2023-02-08 17:33:41 +0000

    generator done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronoun = ["the", "our"];
+
+  const adj = ["great", "big"];
+
+  const noun = ["jogger", "racoon"];
+
+  const dom = [".com", ".es", ".net"];
+
+  for (let indexPronoun = 0; indexPronoun < pronoun.length; indexPronoun++) {
+    for (let indexAdj = 0; indexAdj < adj.length; indexAdj++) {
+      for (let indexNoun = 0; indexNoun < noun.length; indexNoun++) {
+        for (let indexDom = 0; indexDom < dom.length; indexDom++) {
+          console.log(
+            pronoun[indexPronoun] +
+              adj[indexAdj] +
+              noun[indexNoun] +
+              dom[indexDom]
+          );
+        }
+      }
+    }
+  }
 };
